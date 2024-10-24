560f58ee332008899c221fae2879b50849ac8c90
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-10-24 10:34:22 -0300

    :)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function mostraResposta (cartao__conteudo){
+function mostraResposta(cartao__conteudo){
     const resposta = cartao__conteudo.querySelector('.cartao__conteudo__resposta');
     if (resposta.style.display === 'none'){
         resposta.style.display = 'block';
