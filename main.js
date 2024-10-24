4b7f29507a34df3c0355bb23ad305c7c53e61656
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-10-24 10:32:17 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function mostraResposta (cartao__conteudo){
     const resposta = cartao__conteudo.querySelector('.cartao__conteudo__resposta');
-    if (resposta.style.display == 'none'){
-        resposta.style.display == 'block';
+    if (resposta.style.display === 'none'){
+        resposta.style.display = 'block';
     }
     else{
-        resposta.style.display == 'none'
+        resposta.style.display = 'none'
     }
 }
